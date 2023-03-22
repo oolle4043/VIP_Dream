a6184513b941362125581a281645377a66b51a74
Ancha <80835866+oolle4043@users.noreply.github.com>
2023-03-22 09:14:51 +0900

    FastAPI

---
+++ DreamServer/arduino.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter
+from requests import Session
+from fastapi.params import Depends
+from sqlalchemy import desc
+from db import get_db
+
+import models, schemas
+
+router = APIRouter(prefix="/arduino",tags=["arduino"])
+
+@router.post("/device/{user_id}/{device_code}/", status_code=200)
+async def device(user_id: str, device_code: str, db: Session = Depends(get_db)):
+    """
+    디바이스 등록 API
+    """
+    user = db.query(models.User).filter_by(user_id=user_id).first()
+
+    if not user:
+        return {"result": "false"}, 401
+
+    device = db.query(models.Device).filter_by(device_user_id=user.id).first()
+
+    if device:
+        db.query(models.Device).filter(models.Device.device_user_id == user.id).update({"device_code":device_code})
+        return {"result": "200"}, 200
+
+@router.post("/arduino/{user_id}/{temperature}/{humidity}/{air_pollution}/{fine_dust}/", status_code=200)
+async def arduino(user_id: str, temperature: int, humidity: int, air_pollution: int, fine_dust: int, db: Session = Depends(get_db)):
+    """
+    센서 API
+    """
+    user = db.query(models.User).filter_by(user_id=user_id).first()
+    if not user:
+        return {"result": "false"}, 401
+
+    device = db.query(models.Device).filter_by(device_user_id=user.id).first()
+    if not device:
+        return {"result": "false"}, 401
+
+    models.Arduino.create(db, auto_commit=True, arduino_user_id=device.id, temperature=temperature, humidity=humidity, air_pollution=air_pollution, fine_dust = fine_dust)
+    return {"result": "200"}, 200
+
+@router.get("/arduino/{user_id}/{device_code}/", status_code=200)
+async def alarm_status(user_id:str, device_code: str, db:Session = Depends(get_db)):
+    """
+    알람 On, Off API
+    """
+    exist_user = db.query(models.User).filter_by(user_id=user_id).first()
+    if not exist_user:
+            return {"result": "FALSE"}, 401
+
+    device = db.query(models.Device).filter_by(device_code=device_code, device_user_id=exist_user.id).first()
+    if not device:
+        return {"result": "FALSE"}, 401
+    
+    return device.alarm_status, 200
